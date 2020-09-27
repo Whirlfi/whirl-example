19bf643980978c0e3a6c85003ce0536e7c50ef61
vasa <vasa@signy.io>
2020-09-27 12:15:16 +0530

    feat: swap get test
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Web3 from "web3";
+
+const test = async () => {
+  await window.ethereum.enable();
+  const web3 = new Web3(window.web3.currentProvider);
+  const { Swap } = window.WhirlSdk.default;
+  const swap = new Swap(web3);
+  const rate = await swap.getExchangeRate("", "", 1, 2);
+  console.log(rate);
+  // const trx = await swap.swap("USDC", "DAI", 10, 1);
+  // console.log(trx);
+};
 
 function App() {
+  test();
   return (
     <div className="App">
       <header className="App-header">
